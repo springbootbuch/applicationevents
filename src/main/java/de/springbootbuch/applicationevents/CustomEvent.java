198df3a188069bb2a13ed80632ae8ab82f1ace04
Michael J. Simons <michael@simons.ac>
2017-09-18 16:36:33 +0200

    Remove sysouts

--- src/main/java/de/springbootbuch/applicationevents/CustomEvent.java
+++ src/main/java/de/springbootbuch/applicationevents/CustomEvent.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package de.springbootbuch.applicationevents;
 
 import static de.springbootbuch.applicationevents.Application.LOG;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-
 
 public class CustomEvent {	
 	
@@ -20,6 +13,7 @@ public class CustomEvent {
 		this.msg = msg;
 	}	
 }
+
 @Component
 class CustomEventListener {
 	@EventListener
@@ -32,7 +26,6 @@ class DemoComponent {
 	public DemoComponent(ApplicationEventPublisher applicationEventPublisher) {
 		applicationEventPublisher
 			.publishEvent(new CustomEvent("DemoComponent initialized"));
-		System.out.println("what??!");
 	}
 }
 
